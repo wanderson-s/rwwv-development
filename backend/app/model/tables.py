5881e29586ac6d5f461996015afb6144a65547ba
wanderson-s <wanderson.santos@aluno.faculdadeimpacta.com.br>
2021-08-22 18:06:23 -0300

    mapping

--- backend/app/model/tables.py
+++ backend/app/model/tables.py
@@ -13,20 +13,65 @@ class EmploymentType(enum.Enum):
     director = "director"
 
 
-position = sqlalchemy.Table(
-    "positions",
+Employee = sqlalchemy.Table(
+    "employees",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-    sqlalchemy.Column("name", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("first_name", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("last_name", sqlalchemy.String(100), nullable=False),
+    sqlalchemy.Column("cpf", sqlalchemy.String(256), nullable=False),
+    sqlalchemy.Column("position", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("birth_date", sqlalchemy.Date, nullable=False),
+    sqlalchemy.Column("fk_id_permission", sqlalchemy.BigInteger, nullable=Flase),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
     sqlalchemy.Column(
-        "employment_type",
-        sqlalchemy.Enum(EmploymentType),
-        default=EmploymentType.analyst,
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
         nullable=False,
     ),
-    sqlalchemy.Column(
-        "created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_permission"],
+        refcolumns=["permission.id"],
+        name="fk_employees_permission_id",
     ),
+)
+
+System_user = sqlalchemy.Table(
+    "system_users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("email", sqlalchemy.String(256), nullable=False),
+    sqlalchemy.Column("password", sqlalchemy.String(36), nullable=False),
+    sqlalchemy.Column("fk_id_employees", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("fk_id_token", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_employees"],
+        refcolumns=["employees.id"],
+        name="fk_system_users_employees_id",
+    ),
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_token"], refcolumns=["token.id"], name="fk_system_users_token_id"
+    ),
+)
+
+Token = sqlalchemy.Table(
+    "token",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("authorize", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("authoration", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("refresh", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
     sqlalchemy.Column(
         "updated_at",
         sqlalchemy.DateTime,
@@ -36,147 +81,145 @@ position = sqlalchemy.Table(
     ),
 )
 
-"""
-    employee = sqlalchemy.Table(
-        "employees",
-        metadata,
-        sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-        sqlalchemy.Column("first_name", sqlalchemy.String(50), nullable=False),
-        sqlalchemy.Column("last_name", sqlalchemy.String(100), nullable=False),
-        sqlalchemy.Column("email", sqlalchemy.String(256), nullable=False),
-        sqlalchemy.Column("cpf", sqlalchemy.String(11), nullable=False),
-        sqlalchemy.Column("birth_sqlalchemy.Date", sqlalchemy.Date, nullable=True),
-        sqlalchemy.Column("positions_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
-        sqlalchemy.Column(
-            "updated_at",
-            sqlalchemy.DateTime,
-            default=datetime.now,
-            onupdate=datetime.now,
-            nullable=False,
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            columns=["positions_id_fk"], refcolumns=["positions.id"], name="fk_employees_positions_id"
-        ),
-    )
-
-    system_user = sqlalchemy.Table(
-        "system_users",
-        metadata,
-        sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-        sqlalchemy.Column("email", sqlalchemy.String(256), nullable=False),
-        sqlalchemy.Column("password", sqlalchemy.String(36), nullable=False),
-        sqlalchemy.Column("employees_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
-        sqlalchemy.Column(
-            "upsqlalchemy.Dated_at",
-            sqlalchemy.sqlalchemy.DateTime,
-            default=sqlalchemy.sqlalchemy.DateTime.now,
-            onupsqlalchemy.Date=sqlalchemy.sqlalchemy.DateTime.now,
-            nullable=False,
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            sqlalchemy.Columns=["employees_id_fk"],
-            refsqlalchemy.Columns=["employees.id"],
-            name="fk_system_users_employees_id",
-        ),
-    )
-
-    account_employee = sqlalchemy.Table(
-        "accounts_employees",
-        metadata,
-        sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-        sqlalchemy.Column("accounts_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("employees_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
-        sqlalchemy.Column(
-            "upsqlalchemy.Dated_at",
-            sqlalchemy.sqlalchemy.DateTime,
-            default=sqlalchemy.sqlalchemy.DateTime.now,
-            onupsqlalchemy.Date=sqlalchemy.sqlalchemy.DateTime.now,
-            nullable=False,
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            sqlalchemy.Columns=["employees_id_fk"],
-            refsqlalchemy.Columns=["employees.id"],
-            name="fk_account_employees_employees_id",
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            sqlalchemy.Columns=["accounts_id_fk"],
-            refsqlalchemy.Columns=["accounts.id"],
-            name="fk_accounts_accounts_employees_id",
-        ),
-    )
-
-
-    account = sqlalchemy.Table(
-        "accounts",
-        metadata,
-        sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-        sqlalchemy.Column("name", sqlalchemy.String(300), nullable=False),
-        sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
-        sqlalchemy.Column(
-            "upsqlalchemy.Dated_at",
-            sqlalchemy.sqlalchemy.DateTime,
-            default=sqlalchemy.sqlalchemy.DateTime.now,
-            onupsqlalchemy.Date=sqlalchemy.sqlalchemy.DateTime.now,
-            nullable=False,
-        ),
-    )
-
-    budget_status = sqlalchemy.Table(
-        "budget_status",
-        metadata,
-        sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-        sqlalchemy.Column("status", sqlalchemy.String(50), nullable=False),
-        sqlalchemy.Column("accounts_employees_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("budget_id_fk", sqlalchemy.BigInteger, nullable=False),
-        sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
-        sqlalchemy.Column(
-            "upsqlalchemy.Dated_at",
-            sqlalchemy.sqlalchemy.DateTime,
-            default=sqlalchemy.sqlalchemy.DateTime.now,
-            onupsqlalchemy.Date=sqlalchemy.sqlalchemy.DateTime.now,
-            nullable=False,
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            sqlalchemy.Columns=["accounts_employees_id_fk"],
-            refsqlalchemy.Columns=["accounts_employees.id"],
-            name="fk_budget_status_account_employees_id",
-        ),
-        sqlalchemy.ForeignKeyConstraint(
-            sqlalchemy.Columns=["budget_id_fk"], refsqlalchemy.Columns=["budget.id"], name="fk_budget_status_budget_id"
-        ),
-    )
-
-    budget = sqlalchemy.Table(
-    "budget",
+Permission = sqlalchemy.Table(
+    "permission",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
-    sqlalchemy.Column("january", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("february", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("march", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("april", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("may", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("june", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("july", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("august", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("september", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("october", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("november", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("december", Numeric(18, 2), nullable=False),
-    sqlalchemy.Column("type", sqlalchemy.String(30), nullable=False),
-    sqlalchemy.Column("description", Text, nullable=False),
-    sqlalchemy.Column("comment", Text, nullable=True),
-    sqlalchemy.Column("created_at", sqlalchemy.sqlalchemy.DateTime, default=sqlalchemy.sqlalchemy.DateTime.now, nullable=False),
+    sqlalchemy.Column("can_simulate_budget", sqlalchemy.Boolean, nullable=False),
+    sqlalchemy.Column("can_submit_budget", sqlalchemy.Boolean, nullable=False),
+    sqlalchemy.Column("can_approve_budget", sqlalchemy.Boolean, nullable=False),
+    sqlalchemy.Column("can_read_budget", sqlalchemy.Boolean, nullable=False),
+    sqlalchemy.Column("is_admin", sqlalchemy.Boolean, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
     sqlalchemy.Column(
-        "upsqlalchemy.Dated_at",
-        sqlalchemy.sqlalchemy.DateTime,
-        default=sqlalchemy.sqlalchemy.DateTime.now,
-        onupsqlalchemy.Date=sqlalchemy.sqlalchemy.DateTime.now,
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
         nullable=False,
     ),
 )
-"""
+
+Business_unit = sqlalchemy.Table(
+    "business_unit",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("name", sqlalchemy.String(300), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("fk_id_employees", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_employees"],
+        refcolumns=["employees.id"],
+        name="fk_business_unit_employees_id",
+    ),
+)
+
+Budget = sqlalchemy.Table(
+    "budget",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("fk_id_employees", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("fk_id_business_unit", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_employees"], refcolumns=["employees.id"], name="fk_budget_employees_id"
+    ),
+    sqlalchemy.ForeingKeyConstraint(
+        columns=["fk_id_business_unit"],
+        refcolumns=["business_unit.id"],
+        name="fk_budget_business_unit_id",
+    ),
+)
+
+Base_budget = sqlalchemy.Table(
+    "base_budget",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("january", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("february", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("march", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("april", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("may", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("june", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("july", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("august", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("september", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("october", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("november", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("december", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("total", sqlalchemy.Numeric(18, 2), nullable=False),
+    sqlalchemy.Column("type", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("comment", sqlalchemy.Text, nullable=False),
+    sqlalchemy.Column("fk_id_budget", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyconstraint(
+        columns=["fk_id_budget"], refcolumns=["budget.id"], name="fk_base_budget_budget_id"
+    ),
+)
+
+Approver = sqlalchemy.Table(
+    "approvers",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, nullable=True),
+    sqlalchemy.Column("fk_id_budget", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("fk_id_employees", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyconstraint(
+        columns=["fk_id_budget"], refcolumns=["budget.id"], name="fk_approvers_budget_id"
+    ),
+    sqlalchemy.ForeingKeyconstraint(
+        columns=["fk_id_employess"], refcolumns=["employees.id"], name="fk_approvers_employees_id"
+    ),
+)
+
+Status_budget = sqlalchemy.Table(
+    "status_budget",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.BigInteger, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("status", sqlalchemy.String(50), nullable=False),
+    sqlalchemy.Column("approved", sqlalchemy.boolean, nullable=False),
+    sqlalchemy.Column("fk_id_budget", sqlalchemy.BigInteger, nullable=False),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, default=datetime.now, nullable=False),
+    sqlalchemy.Column(
+        "updated_at",
+        sqlalchemy.DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    ),
+    sqlalchemy.ForeingKeyconstraint(
+        columns=["fk_id_budget"], refcolumns=["budget.id"], name="fk_status_budget_budget_id"
+    ),
+)
 # metadata.drop_all(bind=engine)
 metadata.create_all(bind=engine)
