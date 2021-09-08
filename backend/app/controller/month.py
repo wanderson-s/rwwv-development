fcb4e167eaa6b0cf0219c4f21a4cabb501c0fe2c
carlos-rian <65134623+carlos-rian@users.noreply.github.com>
2021-09-08 20:58:00 -0300

    add enpoinst of select budget and month

--- backend/app/controller/month.py
+++ backend/app/controller/month.py
@@ -1,8 +1,8 @@
+from app.schema.output.budget import BaseModelBudget
 from fastapi import HTTPException, status
 from app.schema.output.month import BaseModelMonth
 from sqlalchemy.orm.session import Session
-from app.model.tables import Month
-from app.model.enum import EnumMonthType, EnumMonths
+from app.model.tables import Budget, Month
 from app.schema.input.month import BaseMonth
 from app.controller.budget import select_budget_by_budget_id
 from app.controller.bu import select_bu
@@ -30,3 +30,17 @@ def insert_month(mon: BaseMonth, db: Session) -> BaseModelMonth:
     db.commit()
     db.refresh(month)
     return month
+
+
+def select_month(id: int, db: Session) -> BaseModelMonth:
+    data = db.query(Month).filter(Month.id == id).first()
+    return data
+
+
+def select_month_by_budget_id(budget_id: int, db: Session) -> BaseModelBudget:
+    data = db.query(Budget).filter(Budget.id == budget_id).first()
+    return data
+
+
+def update_month(id: int, mon: BaseMonth, db: Session) -> BaseModelMonth:
+    ...
